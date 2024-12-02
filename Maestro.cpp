74ab75e84e33ad35fd7fd7d24f161fa09faf0e1b
Valeria Porcayo <A01424758@tec.mx>
2024-12-01 21:42:09 -0600

    clase maestro

--- Maestro.cpp
+++ Maestro.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "Maestro.h"
+#include "Maestro.h"
+#include <iostream>
+
+
+Maestro::Maestro(const std::string& nombre, int id) : nombre(nombre), id(id) {}
+
+
+void Maestro::asignarMateria(const Materia& materia) {
+    materiasAsignadas.push_back(materia);
+    std::cout << "Materia " << materia.getNombre() << " asignada a " << nombre << ".\n";
+}
+
+
+void Maestro::asignarCalificacion(const std::string& nombreMateria, int idAlumno, double calificacion) {
+    for (auto& materia : materiasAsignadas) {
+        if (materia.getNombre() == nombreMateria) {
+            materia.asignarCalificacion(idAlumno, calificacion);
+            std::cout << "Calificación asignada en " << nombreMateria << " al alumno ID: " << idAlumno << ".\n";
+            return;
+        }
+    }
+    std::cout << "Materia no encontrada.\n";
+}
+
+void Maestro::verListaAlumnos() {
+    for (const auto& materia : materiasAsignadas) {
+        std::cout << "Materia: " << materia.getNombre() << "\n";
+        materia.verAlumnos();
+    }
+}
+
+std::string Maestro::getNombre() const {
+    return nombre;
+}
+
+int Maestro::getId() const {
+    return id;
+}
+
+std::vector<Materia> Maestro::getMaterias() const {
+    return materiasAsignadas;
+}
--- Maestro.h
+++ Maestro.h
@@ -4,13 +4,27 @@
 
 #ifndef MAESTRO_H
 #define MAESTRO_H
-
-
+#include <string>
+#include <vector>
+#include "Materia.h"
 
 class Maestro {
+private:
+    std::string nombre;
+    int id;
+    std::vector<Materia> materiasAsignadas;
 
+public:
+    Maestro(const std::string& nombre, int id);
+    void asignarMateria(const Materia& materia);
+    void asignarCalificacion(const std::string& nombreMateria, int idAlumno, double calificacion);
+    void verListaAlumnos();
+
+    std::string getNombre() const;
+    int getId() const;
+    std::vector<Materia> getMaterias() const;
 };
 
-
-
 #endif //MAESTRO_H
+
+
