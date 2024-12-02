bae7724cd51d755cf1c27bcf6475898fedf823c9
Valeria Porcayo <A01424758@tec.mx>
2024-12-01 20:54:08 -0600

    si

---
+++ Maestro.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Valeria Porcayo on 01/12/24.
+//
+
+#include "Maestro.h"
---
+++ Maestro.h
@@ -0,0 +1,16 @@
+//
+// Created by Valeria Porcayo on 01/12/24.
+//
+
+#ifndef MAESTRO_H
+#define MAESTRO_H
+
+
+
+class Maestro {
+
+};
+
+
+
+#endif //MAESTRO_H
